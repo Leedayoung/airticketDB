d579a8749f4c7929b6aab8da67acd9419007cbac
Napping <Napping@Napping-Laptop>
2018-06-08 04:27:17 +0200

    Added some tuples.

--- sqlScripts/airticketTable.sql
+++ sqlScripts/airticketTable.sql
@@ -10,3 +10,7 @@ CREATE TABLE airticket(
 
 	primary key(CID,LID,TDTime)
 );
+
+INSERT INTO airticket(CID, LID, TDTime, TATime, TPrice, TSeat, PDID, PAID)
+	VALUES(1, 180, "2018-03-05 01:01:00", "2018-03-05 07:02:00", 100, 200, 1, 2),
+	(1, 180, "2015-06-07 14:01:01", "2017-06-08 01:02:33", 150, 138, 2, 1);
--- sqlScripts/clientTable.sql
+++ sqlScripts/clientTable.sql
@@ -5,3 +5,8 @@ CREATE TABLE client(
 	CGender BOOLEAN NOT NULL,
 	CNation VARCHAR(30) NOT NULL
 );
+
+INSERT INTO client(CID, CName, CAge, CGender, CNation)
+	VALUES (1, "Dayeong", 24, TRUE, "Korean"),
+	(2, "Hojun", 24, FALSE, "Korean"),
+	(3, "Danniel", 25, FALSE, "British")
